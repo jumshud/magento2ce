f82caf2eb469ec3426218737f9fae2cbdb5e4d3b
jumshud <cumshud.mecidli@gmail.com>
2019-10-11 09:46:44 +0300

    reviev comments fixed

--- app/code/Epam/Configuration/Controller/Index/Index.php
+++ app/code/Epam/Configuration/Controller/Index/Index.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types=1);
 
 namespace Epam\Configuration\Controller\Index;
 
+use Epam\Configuration\Model\Store\Config;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Epam\Configuration\Model\Store\Config;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
@@ -14,6 +15,10 @@ class Index extends Action
     */
     private $config;
 
+    /**
+     * @param Context $context
+     * @param Config $config
+     */
     public function __construct(Context $context, Config $config)
     {
         parent::__construct($context);
